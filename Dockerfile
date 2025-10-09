8b411a92b28c0ed7dd212c533fb8ef711d3ce973
Paolo Ruiz <paolo@example.com>
2025-10-09 17:33:15 -0600

    feat: Hola mundo

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+From python:3.12.2-alpine3.21
---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+# Cargar variables de entorno
+load_dotenv()
+
+# Crear instancia de Flask
+app = Flask(__name__)
+
+# Ruta raíz
+@app.route('/')
+def index():
+    return 'Hola Mundo'
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
